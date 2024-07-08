dbafc80fc106da5955581c4ce63e55a0d6384da4
Mach-web <mkanderson771@gmail.com>
2024-07-08 13:05:14 +0300

    Varibles and print

---
+++ variables.dart
@@ -0,0 +1,12 @@
+void main(){
+    var name = "Titanic";
+    var year = 1926;
+    var antennaDiameter = 13.7;
+    var flyByObjects = ['Mercury', 'Venus', "Saturn", "Pluto"];
+
+    var image = {
+        'tags': ['saturn'],
+        'url': '//path/to/saturn.jpg'
+    };
+    print("Name: $name \nYear: $year \nAntenna Dimeter: $antennaDiameter \nDictionary: $flyByObjects");
+}
