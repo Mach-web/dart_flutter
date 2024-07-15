03d0003683beeb54a9238b705a013921418aaee7
Mach-web <mkanderson771@gmail.com>
2024-07-15 19:42:12 +0300

    Different data types in dart

---
+++ built_in_types.dart
@@ -0,0 +1,13 @@
+// import 'package:test/test.dart';
+
+var x = 1;
+var hex = 0xDEADBEEF;
+
+var y = 1.1;
+var exponents = 1.42e5;
+
+void main(){
+    num x = 1;
+    x += 2.5;
+    print("Exponents: $exponents Num datatype: $x");
+}
