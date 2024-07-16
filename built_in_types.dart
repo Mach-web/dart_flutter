c2092b345fbdfaf093addd2c067c1d7fc45822ae
Mach-web <mkanderson771@gmail.com>
2024-07-16 18:15:28 +0300

    const - string var - do not work in const string

--- built_in_types.dart
+++ built_in_types.dart
@@ -40,4 +40,18 @@ multiple lines""";
     print(s);
     s = r"In a raw string even \n does not get special treatment.";
     print(s);
+    // These work in a const string.
+    const aConstNum = 0;
+    const aConstBool = true;
+    const aConstString = 'a constant string';
+
+    // These do NOT work in a const string.
+    var aNum = 0;
+    var aBool = true;
+    var aString = 'a string';
+    const aConstList = [1, 2, 3];
+
+    const validConstString = '$aConstNum $aConstBool $aConstString';
+    // const invalidConstString = '$aNum $aBool $aString $aConstList';
+    print(validConstString);
 }
