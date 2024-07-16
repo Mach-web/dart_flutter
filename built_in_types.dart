907e6bbba7825df2fb4c48edf991d5afed395e9f
Mach-web <mkanderson771@gmail.com>
2024-07-16 17:03:38 +0300

    Changing between different data types

--- built_in_types.dart
+++ built_in_types.dart
@@ -1,5 +1,7 @@
 // import 'package:test/test.dart';
 
+import 'dart:ffi';
+
 var x = 1;
 var hex = 0xDEADBEEF;
 
@@ -10,4 +12,11 @@ void main(){
     num x = 1;
     x += 2.5;
     print("Exponents: $exponents Num datatype: $x");
+    // string into number
+    String strNo = "1";
+    var int_number = int.parse(strNo);
+    var double_number = double.parse(strNo);
+    var str_int_number = 856.toString();
+    var str_double_number = 1.3567.toStringAsFixed(3);
+    print("String: $strNo Integer: $int_number Double: $double_number int to string: $str_int_number Double to string: $str_double_number");
 }
