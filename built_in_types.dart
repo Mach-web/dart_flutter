2a9c2c575237797362558a4ef28563a087017624
Mach-web <mkanderson771@gmail.com>
2024-07-16 17:13:20 +0300

    Bitwise operations

--- built_in_types.dart
+++ built_in_types.dart
@@ -19,4 +19,8 @@ void main(){
     var str_int_number = 856.toString();
     var str_double_number = 1.3567.toStringAsFixed(3);
     print("String: $strNo Integer: $int_number Double: $double_number int to string: $str_int_number Double to string: $str_double_number");
+    // Bitwise operations
+    assert((3 << 1) == 6); // 0011 << 1 == 0110
+    assert((3 | 4) == 7); // 0011 | 0100 == 0111
+    print("Bitwise operations(3|4): ${3|4}  (7&3): ${7&3}");
 }
