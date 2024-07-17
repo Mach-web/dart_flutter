89835e153dfab8fa5a760a2a6b17bd65d50e71f2
Mach-web <mkanderson771@gmail.com>
2024-07-17 13:47:41 +0300

    Unicode characters

--- unicode.dart
+++ unicode.dart
@@ -1,8 +1,10 @@
-// import 'package:characters/characters.dart';
+import 'package:characters/characters.dart';
 
 void main(){
     var hello = 'Hi 🇩🇰';
     print(hello);
-    print("The end of string is: ${hello.substring(hello.length - 1)}");
-    print(hello.length);
+    print("The end of string is: ${hello.substring(hello.length - 1)}        ${hello.substring(3)}");
+    print("The last character is: ${hello.characters.last}");
+    print(#radix);
+    print(#bar);
 }
