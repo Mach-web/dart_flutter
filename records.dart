7d86902b6af795150bad7bfbec1eda1d1a69ef72
Mach-web <mkanderson771@gmail.com>
2024-07-17 19:23:54 +0300

    Working with json

--- records.dart
+++ records.dart
@@ -3,6 +3,9 @@
     return (b, a);
 }
 
+(String name, int age, String color) userInfo (Map<String, dynamic> json){
+  return (json['name'] as String, json['age'] as int, json['color'] as String);
+}
 void main(){
   var record = ('first', a:1, b:true, 'last');
   print("Example record: $record \t ${record.$1} \t ${record.a} \t ${record.b} \t ${record.$2}");
@@ -19,5 +22,20 @@ void main(){
   
   (int x, int y, String z) recordA = (2, 3, "Hello");
   (int a, int b, String C) recordB = (2, 3, "Hello");
-  print("(int x, int y, String z) == (int a, int b, String C) = (2, 3, 'Hello') is ${recordA == recordB}"); 
+  print("(int x, int y, String z) == (int a, int b, String C) is ${recordA == recordB}"); 
+
+  ({int x, int y, int z}) point = (x: 1, y: 2, z: 3);
+  ({int r, int g, int b}) colour = (r: 1, g: 2, b: 3);
+
+  print("({int x, int y, int z}) == ({int r, int g, int b}) is ${point == colour}"); // Prints 'false'. Lint: Equals on unrelated types.
+
+  final json = <String, dynamic>{
+    'name': "MachTechie",
+    'age':21,
+    'color':'blue',
+  };
+
+  var(name, age, color) = userInfo(json);
+  print("Working with json: $json $name, $age, $color");
+
 }
