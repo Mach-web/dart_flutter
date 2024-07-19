2be822abcead2fefa542263bb4622316c50854ce
Mach-web <mkanderson771@gmail.com>
2024-07-19 19:14:00 +0300

    Create a set from list

--- generics.dart
+++ generics.dart
@@ -3,4 +3,18 @@ void main(){
     str_list.addAll(['Hello', "There", "!!!"]);
     str_list.add('42');
     print("The 42 had to be a string: $str_list");
+    var names = <String>['Seth', 'Kathy', 'Lars'];
+    var uniqueNames = <String>{'Seth', 'Kathy', 'Lars'};
+    var pages = <String, String>{
+    'index.html': 'Homepage',
+    'robots.txt': 'Hints for web robots',
+    'humans.txt': 'We are people, not machines'
+    };
+    print("Using generics for collection literals: $pages");
+    var nameSet = Set<String>.from(names);
+    print("Create set from list: $nameSet");
+    var views = Map<int, String>();
+    views[1] = 'first';
+    views[2] = 'second';
+    print(views);
 }
