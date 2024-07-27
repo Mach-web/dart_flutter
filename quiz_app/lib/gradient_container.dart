aba9f7c2a82db56bd0a47464ee710bed188ff81a
Mach-web <mkanderson771@gmail.com>
2024-07-27 11:32:53 +0300

    Added a solid color

---
+++ quiz_app/lib/gradient_container.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget{
+  const GradientContainer({super.key});
+  @override
+  Widget build(context){
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.deepPurpleAccent
+        ),
+    );
+  }
+}
