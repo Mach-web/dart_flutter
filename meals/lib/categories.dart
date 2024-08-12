e871b172cf3a8414441fda5a4237de05f7aa85e7
Mach-web <mkanderson771@gmail.com>
2024-08-12 14:51:24 +0300

    Designed categories

---
+++ meals/lib/categories.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget{
+  const CategoriesScreen({super.key});
+
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("What's your taste"),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.5,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
+        children: [
+          Text("1")
+        ],
+        ),
+    );
+  }
+}
