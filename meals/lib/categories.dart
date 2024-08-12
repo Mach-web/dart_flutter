a170e59d20b7d8186a260c978578f901cff56191
Mach-web <mkanderson771@gmail.com>
2024-08-12 14:54:10 +0300

    USing a GRidView

--- meals/lib/categories.dart
+++ meals/lib/categories.dart
@@ -15,8 +15,15 @@ class CategoriesScreen extends StatelessWidget{
           childAspectRatio: 1.5,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20),
-        children: [
-          Text("1")
+        children: const [
+          Text("1", style: TextStyle(color: Colors.white),),
+          Text("2", style: TextStyle(color: Colors.white),),
+          Text("3", style: TextStyle(color: Colors.white),),
+          Text("4", style: TextStyle(color: Colors.white),),
+          Text("5", style: TextStyle(color: Colors.white),),
+          Text("6", style: TextStyle(color: Colors.white),),
+          Text("7", style: TextStyle(color: Colors.white),),
+          Text("8", style: TextStyle(color: Colors.white),),
         ],
         ),
     );
