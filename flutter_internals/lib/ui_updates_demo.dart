67d18bd9ac73592978c5cc8e0e32eb3ae727a78d
Mach-web <mkanderson771@gmail.com>
2024-08-12 13:57:54 +0300

    Uderstanding and using keys

--- flutter_internals/lib/ui_updates_demo.dart
+++ flutter_internals/lib/ui_updates_demo.dart
@@ -29,12 +29,12 @@ class _UIUpdatesDemo extends State<UIUpdatesDemo> {
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
+             Text(
               'Every Flutter developer should have a basic understanding of Flutter\'s internals!',
               textAlign: TextAlign.center,
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 16),
+             SizedBox(height: 16),
              Text(
               'Do you understand how Flutter updates UIs?',
               textAlign: TextAlign.center,
--- fluttter_internals/android/app/src/main/kotlin/com/example/fluttter_internals/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.fluttter_internals
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
