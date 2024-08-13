6615ca3ad01244ad927f0968196a58af0418376c
Mach-web <mkanderson771@gmail.com>
2024-08-13 19:55:23 +0300

    Introducing the stack widget

---
+++ meals/lib/widgets/meal_item.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class MealItem extends StatelessWidget{
+  const MealItem({super.key, required this.meal});
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) =>
+  Card(
+    margin: const EdgeInsets.all(8),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(15)
+    ),
+    // apply contents to have the border
+    clipBehavior: Clip.hardEdge,
+    // 3D effect-shadow
+    elevation: 2,
+    child: InkWell(
+      onTap: (){},
+      child: Stack(
+        children: [
+          FadeInImage(
+            placeholder: MemoryImage(kTransparentImage), 
+            image: NetworkImage(meal.imageUrl),
+            fit: BoxFit.cover,
+            height: 200,
+            width: double.infinity,
+            ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              color: Colors.black54,
+              padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 44),
+              child: Column(
+                children: [
+                  Text(
+                    meal.title,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    textAlign: TextAlign.center,
+                    softWrap: true,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                    ),
+                ],
+              ),
+            )),
+        ],
+      ),
+    )
+  );
+}
