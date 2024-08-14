a759c184a416808b0ca6c84e6e79d8ab737979f2
Mach-web <mkanderson771@gmail.com>
2024-08-15 01:22:46 +0300

    Add a switch list tile

---
+++ meals/lib/screens/filters.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class FiltersScreen extends StatefulWidget{
+  const FiltersScreen({super.key});
+  @override  
+  State<FiltersScreen> createState(){
+    return _FiltersScreenState();
+  }
+}
+
+class _FiltersScreenState extends State<FiltersScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Your filter"),
+      ),
+      body: Column(
+        children: [
+          SwitchListTile(
+            value: value, 
+            onChanged: onChanged,
+            title: Text(
+              "Gluten free",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface
+              ),
+            ),
+            subtitle: Text(
+              "Only include glutten free meals",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface
+              ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
+          ),
+        ],
+      ),
+    );
+  }
+}
