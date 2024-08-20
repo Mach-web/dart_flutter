8cabc9c7f50dac7c9d0624715962063a7033b17d
Mach-web <mkanderson771@gmail.com>
2024-08-20 21:22:41 +0300

    Use onSaved function to access variables when formKey is saved

--- shopping_list/lib/screens/new_item.dart
+++ shopping_list/lib/screens/new_item.dart
@@ -10,9 +10,13 @@ class NewItem extends StatefulWidget{
 
 class _NewItemState extends State<NewItem>{
   final _formKey = GlobalKey<FormState>();
+  String _enteredName = "";
 
   void _saveItem(){
-    _formKey.currentState!.validate();
+    if(_formKey.currentState!.validate()){
+      _formKey.currentState!.save();
+      print(_enteredName);
+    };
   }
   @override
   Widget build(BuildContext context) {
@@ -37,6 +41,7 @@ class _NewItemState extends State<NewItem>{
                   }
                   return null;
                 },
+                onSaved: (value)=>_enteredName = value!
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
